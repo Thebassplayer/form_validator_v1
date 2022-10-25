f4f559b340055af304c48fd3c1ad72b4d072cf98
Roy Lopez <windup.dues-0f@icloud.com>
2022-10-25 17:35:18 -0300

    HtmlWebpackPlugin install

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
@@ -6,6 +8,7 @@ module.exports = {
     filename: "main.[contenthash].js",
     path: path.resolve(__dirname, "dist"),
   },
+  plugins: [new HtmlWebpackPlugin()],
   module: {
     rules: [
       {
